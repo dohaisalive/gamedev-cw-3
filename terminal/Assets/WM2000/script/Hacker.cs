23198e54f090634f0aeaace89a37d7195cb7f9fe
dohaisalive <dohaisalive@gmail.com>
2020-12-23 18:38:09 +0300

    terminal basics

---
+++ terminal/Assets/WM2000/script/Hacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Showmainmenu("hewwo");
+        
+    }
+
+    void Showmainmenu(string greeting)
+    {
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("press 1 to get inside area 51");
+        Terminal.WriteLine("press 2 to get inside YOUR MIND");
+    }
+    
+}

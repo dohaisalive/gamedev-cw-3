c7752c4a388fd18b8d3e9c466ebb3610d5b09923
dohaisalive <dohaisalive@gmail.com>
2020-12-23 19:52:35 +0300

    testpassword

--- terminal/Assets/WM2000/script/Hacker.cs
+++ terminal/Assets/WM2000/script/Hacker.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    int level;
+    string testpass;
+    string currentscreen = "mainmenu"; //to save screen
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,4 +22,68 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("press 2 to get inside YOUR MIND");
     }
     
+    void OnUserInput(string input)
+    {
+        if (currentscreen == "mainmenu")
+        {
+            Setlevel(input);
+        }
+        else if (currentscreen == "passwordscreen")
+        {
+            Checkpassword(input);
+        }
+    }
+
+
+    void Setlevel(string selectedlevel)
+    {
+        if (selectedlevel == "1")
+        {
+            level = 1;
+            Setrandompassword();
+        }
+
+        else if (selectedlevel == "2")
+        {
+            level = 2;
+            Setrandompassword();
+        }
+        
+        else
+        {
+            Terminal.WriteLine("calling the police!");
+        }
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("level: " + level);
+        currentscreen = "passwordscreen";
+    }
+
+    void Setrandompassword()
+    {
+        StartGame();
+
+        switch (level)
+        {
+            case 1:
+                testpass = "safe";
+                Terminal.WriteLine(testpass);
+                break;
+            case 2:
+                testpass = "safe";
+                Terminal.WriteLine(testpass);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Checkpassword(string userinput)
+    {
+        if (userinput == testpass)
+            Terminal.WriteLine("correct! OwO");
+
+    }
 }
